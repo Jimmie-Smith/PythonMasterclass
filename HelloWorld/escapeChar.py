95ad204b31d6ef4e1043eaa3ccd551cf387107ca
Jimmie-Smith <countertenacity@gmail.com>
2022-08-01 21:02:12 -0500

    Learned about assigning strings to variables and escaping symbols

---
+++ HelloWorld/escapeChar.py
@@ -0,0 +1,15 @@
+splitString = "This string has been \nsplit over \nseveral \nlines"
+print(splitString)
+tabbedString = "1\t2\t3\t4\t5"
+print(tabbedString)
+print('the petshop owner said "no, no, \'e \' uh... he\'s resting".')
+
+# If you enclosed the string with a ', the rest of the ' must be escaped. Or just used triple quotes for better readability
+
+print("""The shop owner said "No, no 'e's uh, .... he's resting".""""")
+
+# You can also use triple quotes if you want to break lines instead of using \n like in the first example
+
+print("""The shop owner said
+"No, no 'e's uh, .... 
+he's resting".""""")
---
+++ HelloWorld/strings.py
@@ -0,0 +1,12 @@
+print('today is a good to learn python')
+print('python is fun')
+print("python's strings are easy to use")
+print('we can even use "quotes" in strings')
+print("Hello" + " World")
+greeting = "Hello"
+name = input("please enter name")
+print(greeting + name)
+
+# If we want a space, we can add that too! input prompts a command taken and stored into a variable
+
+print(greeting + ' ' + name)
