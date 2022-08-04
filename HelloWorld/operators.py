564f3227d02eb038076870e8cf026588c102d7f9
Jimmie-Smith <countertenacity@gmail.com>
2022-08-03 21:01:05 -0500

    Extra backslash character. raw strings, numeric data types. numeric operators. expressions. operator coding exercise

---
+++ HelloWorld/operators.py
@@ -0,0 +1,27 @@
+a = 12
+b = 3
+
+
+print(a + b)
+print(a - b)
+print(a * b)
+print(a / b)
+print(a//b)
+print(a % b)
+print()
+
+#for i in range(1, 4):
+  #  print(i)
+    
+#for i in range(1, a//b):
+  #  print(i)
+    
+#buns = 15 // 2.40
+#print('How many buns can i buy?', buns)
+
+days_left_until_payday = 12
+money_in_my_bank_account = 1233.123
+
+
+print("Money is type: ", type(money_in_my_bank_account))
+print('My spending limit everyday: ', money_in_my_bank_account / days_left_until_payday )
--- HelloWorld/strings.py
+++ HelloWorld/strings.py
@@ -1,12 +1,30 @@
-print('today is a good to learn python')
-print('python is fun')
-print("python's strings are easy to use")
-print('we can even use "quotes" in strings')
-print("Hello" + " World")
+#print('today is a good to learn python')
+#print('python is fun')
+#print("python's strings are easy to use")
+#print('we can even use "quotes" in strings')
+#print("Hello" + " World")
 greeting = "Hello"
-name = input("please enter name")
+name = "jimmie"
 print(greeting + name)
 
 # If we want a space, we can add that too! input prompts a command taken and stored into a variable
 
-print(greeting + ' ' + name)
+#print(greeting + ' ' + name)
+
+#print("C:\\Users\\tim\\notes")
+
+#print(r"C:\Users\tim\notes")
+
+age = 24
+age = "2 years"
+floater = 25.5
+print(age)
+print(name +  " is " + age +  " years old")
+
+
+
+print(type(greeting))
+print('This is the new age type', type(age))
+
+
+
